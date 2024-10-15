acf8a0bbdffcd4189b55e5811c7842b7498637ca
LeetManSup <100480254+LeetManSup@users.noreply.github.com>
2024-10-16 01:02:16 +0300

    upd2

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React, {useEffect, useState} from "react";
 import {ethers} from "ethers";
-import {Button, Card, CardHeader} from "react-bootstrap";
+import {Button, Card} from "react-bootstrap";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
-  const [hasMetamask, setHasMetamask] = useState(true);
-  const [isConnected, setIsConnected] = useState(false);
+  const [, setHasMetamask] = useState(true);
+  const [, setIsConnected] = useState(false);
   const [accountAddress, setAccountAddress] = useState('');
   const [accountBalance, setAccountBalance] = useState('');
   const provider = new ethers.providers.Web3Provider(window.ethereum)
@@ -48,9 +48,6 @@ function App() {
 
   return (
     <div className="App">
-      {/* Calling all values which we
-       have stored in usestate */}
-
       <Card className="text-center">
         <Card.Header>
           <strong>Address: </strong>
